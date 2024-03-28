fc682406536a3b2199b78d66f80a3cffcaedc3d4
Amnon Khen <amnon@ebi.ac.uk>
2024-03-28 11:59:50 +0000

    microbe fields in config.js dc-10

--- k8s/overlays/demo/config.js
+++ k8s/overlays/demo/config.js
@@ -1,7 +1,7 @@
 window.appConfig = {
     "ENVIRONMENT": "demo",
     "basename": "/catalogue/demo",
-    "REST_ENDPOINT_URL": '/biosamples/samples?filter=attr%3Aproject+name%3AMICROBE',
+    "REST_ENDPOINT_URL": '/biosamples/samples?filter=attr%3Aproject+name%3AMICROBE&filter=attr%3Acenter&size=200',
     "SCHEMA_ENDPOINT_URL": '/biosamples/schemas/core/sample.json',
     "RESOURCE_JSON_PATH": '_embedded.samples',
     "GRID_CONFIG": [
@@ -12,6 +12,9 @@ window.appConfig = {
             "name": "release",
         },
         {
+            "name": "accession",
+        },
+        {
             "name": "organism",
             valueGetter: params => params.data.characteristics?.organism[0]?.text
         },
@@ -20,23 +23,45 @@ window.appConfig = {
             valueGetter: params => params.data.characteristics?.biome?.[0]?.text
         },
         {
+            "name": "center",
+            valueGetter: params => params.data.characteristics?.center?.[0]?.text
+        },
+        {
+            "name": "freezingMethod",
+            valueGetter: params => params.data.characteristics?.["freezing method"]?.[0]?.text
+        },        {
+            "name": "cultivation",
+            valueGetter: params => params.data.characteristics?.cultivation?.[0]?.text
+        },
+        {
+            "name": "cryoprotectant",
+            valueGetter: params => params.data.characteristics?.["cryoprotectant"]?.[0]?.text
+        },
+        {
             "name": "soilType",
             valueGetter: params => params.data.characteristics?.["soil type"]?.[0].text
         },
         {
-            "name": "collectionDate",
-            valueGetter: params => params.data.characteristics?.["collection date"]?.[0].text
+            "name": "analysisDate",
+            valueGetter: params => params.data.characteristics?.["analysis date"]?.[0].text
         },
-       {
+        {
+            "name": "preservationTemperature",
+            valueGetter: params => params.data.characteristics?.["preservation temperature"]?.[0].text
+        },
+        {
             "name": "broadScaleEnvironmentalContext",
             valueGetter: params => params.data.characteristics?.["broad-scale environmental context"]?.[0].text
-        },       {
+        },
+        {
             "name": "environmentalMedium",
             valueGetter: params => params.data.characteristics?.["environmental medium"]?.[0].text
-        },       {
+        },
+        {
             "name": "regionAndLocality",
             valueGetter: params => params.data.characteristics?.["geographic location (region and locality)"]?.[0].text
-        },       {
+        },
+        {
             "name": "countryAndOrSea))",
             valueGetter: params => params.data.characteristics?.["geographic location (country and/or sea)"]?.[0].text
         },
@@ -59,16 +84,26 @@ window.appConfig = {
             field: "soilType",
             type: "checkbox",
             data_type: "string"
-        }, {
+        },
+        {
             field: "organism",
             type: "checkbox",
             data_type: "string"
         },
         {
-            field: "pH",
+            field: "cultivation",
             type: "checkbox",
-            data_type: "numeric_range",
-            range_interval: 3
+            data_type: "string"
+        },
+       {
+            field: "cryoprotectant",
+            type: "checkbox",
+            data_type: "string"
+        },
+        {
+            field: "freezingMethod",
+            type: "checkbox",
+            data_type: "string"
         }
     ]
 
--- public/config.js
+++ public/config.js
@@ -1,7 +1,7 @@
 window.appConfig = {
     "ENVIRONMENT":"demo",
     "basename":"/",
-    "REST_ENDPOINT_URL": '/biosamples/samples?filter=attr%3Aproject+name%3AMICROBE',    "SCHEMA_ENDPOINT_URL": '/biosamples/schemas/core/sample.json',
+    "REST_ENDPOINT_URL": '/biosamples/samples?filter=attr%3Aproject+name%3AMICROBE&filter=attr%3Acenter&size=200',    "SCHEMA_ENDPOINT_URL": '/biosamples/schemas/core/sample.json',
     "RESOURCE_JSON_PATH": '_embedded.samples',
     "GRID_CONFIG":  [
         {
@@ -9,6 +9,9 @@ window.appConfig = {
         },
         {
             "name": "release",
+        },
+        {
+            "name": "accession",
         },
         {
             "name": "organism",
@@ -19,23 +22,45 @@ window.appConfig = {
             valueGetter: params => params.data.characteristics?.biome?.[0]?.text
         },
         {
+            "name": "center",
+            valueGetter: params => params.data.characteristics?.center?.[0]?.text
+        },
+        {
+            "name": "freezingMethod",
+            valueGetter: params => params.data.characteristics?.["freezing method"]?.[0]?.text
+        },        {
+            "name": "cultivation",
+            valueGetter: params => params.data.characteristics?.cultivation?.[0]?.text
+        },
+        {
+            "name": "cryoprotectant",
+            valueGetter: params => params.data.characteristics?.["cryoprotectant"]?.[0]?.text
+        },
+        {
             "name": "soilType",
             valueGetter: params => params.data.characteristics?.["soil type"]?.[0].text
         },
         {
-            "name": "collectionDate",
-            valueGetter: params => params.data.characteristics?.["collection date"]?.[0].text
+            "name": "analysisDate",
+            valueGetter: params => params.data.characteristics?.["analysis date"]?.[0].text
+        },
+        {
+            "name": "preservationTemperature",
+            valueGetter: params => params.data.characteristics?.["preservation temperature"]?.[0].text
         },
        {
             "name": "broadScaleEnvironmentalContext",
             valueGetter: params => params.data.characteristics?.["broad-scale environmental context"]?.[0].text
-        },       {
+        },
+        {
             "name": "environmentalMedium",
             valueGetter: params => params.data.characteristics?.["environmental medium"]?.[0].text
-        },       {
+        },
+        {
             "name": "regionAndLocality",
             valueGetter: params => params.data.characteristics?.["geographic location (region and locality)"]?.[0].text
-        },       {
+        },
+        {
             "name": "countryAndOrSea))",
             valueGetter: params => params.data.characteristics?.["geographic location (country and/or sea)"]?.[0].text
         },
@@ -58,16 +83,26 @@ window.appConfig = {
             field: "soilType",
             type: "checkbox",
             data_type: "string"
-        }, {
+        },
+        {
             field: "organism",
             type: "checkbox",
             data_type: "string"
         },
-        {
-            field: "pH",
+       {
+            field: "cultivation",
             type: "checkbox",
-            data_type: "numeric_range",
-            range_interval: 1
+            data_type: "string"
+        },
+       {
+            field: "cryoprotectant",
+            type: "checkbox",
+            data_type: "string"
+        },
+        {
+            field: "freezingMethod",
+            type: "checkbox",
+            data_type: "string"
         }
     ]
 
